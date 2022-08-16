d5d15d511151489df539d877134d6ebc4fab1b2d
Denis <denis.kolesnikw@gmail.com>
2022-08-16 14:36:51 +0300

    1. Refactoring classes Forecast,Weather and WeatherBot. 2. Use SOLID

--- WeatherBot.java
+++ WeatherBot.java
@@ -17,6 +17,8 @@ import java.util.List;
 
 
 public class WeatherBot extends TelegramLongPollingBot {
+    private Message message;
+
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
@@ -40,98 +42,98 @@ public class WeatherBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        ModelWeather modelWeather = new ModelWeather();
-        ModelForecast modelForecast = new ModelForecast();
-        Message message = update.getMessage();
+        message = update.getMessage();
 
         if (update.hasMessage()) {
-
+            SendMessage sendMessage = new SendMessage();
             if (message.hasText()) {
                 String text = message.getText();
-                SendMessage sendMessage = new SendMessage();
-//                add "/start" keyboard
-                ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-                sendMessage.setReplyMarkup(replyKeyboardMarkup);
-                replyKeyboardMarkup.setSelective(true);
-                replyKeyboardMarkup.setResizeKeyboard(true);
-                replyKeyboardMarkup.setOneTimeKeyboard(false);
 
-                List<KeyboardRow> keyboardStart = new ArrayList<>();
-                KeyboardRow startButton = new KeyboardRow();
-                startButton.add(new KeyboardButton("/start"));
-                keyboardStart.add(startButton);
-                replyKeyboardMarkup.setKeyboard(keyboardStart);
-
-                sendMessage.setReplyMarkup(replyKeyboardMarkup);
                 if (text.equals("/start")) {
-                    sendMessage.setText("Enter a city, please:");
-                    sendMessage.setChatId(message.getChatId());
-                    try {
-                        execute(sendMessage);
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-
-//                   add InlineButtons and CallBack
-                    sendMessage.setText(message.getText());
-                    sendMessage.setChatId(message.getChatId());
-                    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-                    InlineKeyboardButton currentButton = new InlineKeyboardButton();
-                    currentButton.setText("Current weather");
-                    currentButton.setCallbackData(message.getText() + "current");
-                    InlineKeyboardButton forecastButton = new InlineKeyboardButton();
-                    forecastButton.setText("5-day forecast");
-                    forecastButton.setCallbackData(message.getText() + "forecast");
-                    List<InlineKeyboardButton> buttonsUp = new ArrayList<>();
-                    List<InlineKeyboardButton> buttonsDown = new ArrayList<>();
-                    buttonsUp.add(currentButton);
-                    buttonsDown.add(forecastButton);
-                    List<List<InlineKeyboardButton>> row = new ArrayList<>();
-                    row.add(buttonsUp);
-                    row.add(buttonsDown);
-                    inlineKeyboardMarkup.setKeyboard(row);
-                    sendMessage.setReplyMarkup(inlineKeyboardMarkup);
-                    try {
-                        execute(sendMessage);
-
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
+                    setKeyboardButton(sendMessage);
                 }
+                else setInlineKeyboard(sendMessage);
+
             }
 
         } else if (update.hasCallbackQuery()) {
-//            get CallBack
             long chatId = update.getCallbackQuery().getMessage().getChatId();
             CallbackQuery callbackQuery = update.getCallbackQuery();
             String data = callbackQuery.getData();
-            SendMessage sendMessage = new SendMessage();
+            SendMessage sendMessageCallback = new SendMessage();
             if (data.contains("current")) {
-//              sending current weather
-                sendMessage.setChatId(chatId);
+                sendMessageCallback.setChatId(chatId);
                 try {
-                    sendMessage.setText(Weather.getWeather(data.replace("current", ""), modelWeather));
+                    sendMessageCallback.setText(Weather.getWeather(data.replace("current", "")));
                 } catch (IOException e) {
-                    sendMessage.setText("The city is not found!");
+                    sendMessageCallback.setText("The city is not found!");
                 }
 
             } else if (data.contains("forecast")) {
-//                sending forecast
-                sendMessage.setChatId(chatId);
+                sendMessageCallback.setChatId(chatId);
                 try {
-                    sendMessage.setText(Forecast.getForecast(data.replace("forecast", ""), modelForecast));
+                    sendMessageCallback.setText(Forecast.getForecast(data.replace("forecast", "")));
                 } catch (IOException e) {
-                    sendMessage.setText("The city is not found!");
+                    sendMessageCallback.setText("The city is not found!");
                 }
             }
             try {
-                execute(sendMessage);
+                execute(sendMessageCallback);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
 
+    }
+
+
+    private void setKeyboardButton(SendMessage sendButton) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendButton.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardStart = new ArrayList<>();
+        KeyboardRow startButton = new KeyboardRow();
+        startButton.add(new KeyboardButton("/start"));
+        keyboardStart.add(startButton);
+        replyKeyboardMarkup.setKeyboard(keyboardStart);
+        sendButton.setReplyMarkup(replyKeyboardMarkup);
+        sendButton.setText("Enter a city, please:");
+        sendButton.setChatId(message.getChatId());
+        try {
+            execute(sendButton);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setInlineKeyboard(SendMessage sendInline) {
+        sendInline.setText(message.getText());
+        sendInline.setChatId(message.getChatId());
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton currentButton = new InlineKeyboardButton();
+        currentButton.setText("Current weather");
+        currentButton.setCallbackData(message.getText() + "current");
+        InlineKeyboardButton forecastButton = new InlineKeyboardButton();
+        forecastButton.setText("5-day forecast");
+        forecastButton.setCallbackData(message.getText() + "forecast");
+        List<InlineKeyboardButton> buttonsUp = new ArrayList<>();
+        List<InlineKeyboardButton> buttonsDown = new ArrayList<>();
+        buttonsUp.add(currentButton);
+        buttonsDown.add(forecastButton);
+        List<List<InlineKeyboardButton>> row = new ArrayList<>();
+        row.add(buttonsUp);
+        row.add(buttonsDown);
+        inlineKeyboardMarkup.setKeyboard(row);
+        sendInline.setReplyMarkup(inlineKeyboardMarkup);
+        try {
+            execute(sendInline);
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 }
